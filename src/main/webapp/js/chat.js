3b18e7f5d9ad946c143c6efab3fb2b061f19ed86
knight392 <knight_yya@163.com>
2020-11-20 11:45:33 +0800

    correct item_blank

--- src/main/webapp/js/chat.js
+++ src/main/webapp/js/chat.js
@@ -156,6 +156,81 @@
 
 
     // 登录成功和自动登录后会进行自动与服务器进行连接
+    var lockReconnect = false;//避免重复连接
+    var myMarkNumber = "19"
+    var wsUrl = "ws://192.168.137.105:8080/WebSocket/";
+    var ws;
+    var tt;
+    function createWebSocket() {
+      try {
+        ws = new WebSocket(wsUrl);
+        init();
+      } catch(e) {
+        // console.log('catch');
+        reconnect(wsUrl);
+      }
+    }
+    function init() {
+      ws.onclose = function () {
+        // console.log('链接关闭');
+        reconnect(wsUrl);
+      };
+      ws.onerror = function() {
+        // console.log('发生异常了');
+        reconnect(wsUrl);
+      };
+      ws.onopen = function () {
+        //心跳检测重置
+        heartCheck.start();
+      };
+      ws.onmessage = function (event) {
+        //拿到任何消息都说明当前连接是正常的
+        // console.log('接收到消息');
+        //消息的展示
+        heartCheck.start();
+      }
+      //发送消息
+    }
+    function reconnect(url) {
+      if(lockReconnect) {
+        return;
+      };
+      lockReconnect = true;
+      //没连接上会一直重连，设置延迟避免请求过多
+      tt && clearTimeout(tt);
+      tt = setTimeout(function () {
+        createWebSocket(url);//重新连接，递归
+        lockReconnect = false;
+      }, 4000);
+    }
+    //心跳检测
+    var heartCheck = {
+      timeout: 3000,
+      timeoutObj: null,
+      serverTimeoutObj: null,
+      start: function(){
+        // console.log('start');
+        var self = this;
+        this.timeoutObj && clearTimeout(this.timeoutObj);
+        this.serverTimeoutObj && clearTimeout(this.serverTimeoutObj);
+        this.timeoutObj = setTimeout(function(){
+          //这里发送一个心跳，后端收到后，返回一个心跳消息，
+        //   console.log('55555');
+          ws.send("123456789");//如果是后端还在线，就会触发onmessage事件
+          self.serverTimeoutObj = setTimeout(function() {
+            // console.log(111);
+            // console.log(ws);
+            ws.close();
+            // createWebSocket();
+          }, self.timeout);
+
+        }, this.timeout)
+      }
+    }
+
+    // 创建一个webSocket与服务器进行连接
+    createWebSocket(wsUrl);
+
 
         var meObj = {
             id: "191543214",
